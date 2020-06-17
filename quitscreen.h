80dc5aad87e12ad0bfaf96fcd142060b60de1263
mariem-BM <61388961+mariem-BM@users.noreply.github.com>
2020-06-17 07:55:42 +0100

    Add files via upload

---
+++ quitscreen.h
@@ -0,0 +1,20 @@
+//
+//  quitscreen.h
+//  
+//
+//  
+//
+
+#ifndef quitscreen_h
+#define quitscreen_h
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+#include "test.h"
+#include "menu.h"
+void quitscreen(SDL_Surface* bg,SDL_Surface *screen,personnage p,SDL_Rect camera);
+
+
+#endif /* quitscreen_h */
